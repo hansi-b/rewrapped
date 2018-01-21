5e824792f0aa743376afa1c3f0e8261bf812a00d
Hans Bering <hansi.b.github@gmail.moc>
2018-01-21 12:12:37 +0100

    added breaking and stripping single-value field
    * field evaluation can break(=stop) on None or other value
    * stripping just uses str strip

--- test/examples/optionalGroups.py
+++ test/examples/optionalGroups.py
@@ -55,5 +55,29 @@ class TestSomeGroups(unittest.TestCase):
         r = res[3]
         self.assertEqual((123, None, None), (r.numId, r.idTag, r.alphaNumId))
 
+
+class NumericOrLogicIdWithBreak(ReWrap):
+    matchOn = "([0-9]+)|(id|@)\s+([0-9a-zA-Z]+)"
+
+    numId = matched.g1.breakOnNone.asInt
+    idTag = matched.g2
+    alphaNumId = matched.g3
+
+
+class TestGroupsWithBreak(unittest.TestCase):
+
+    def testSimple(self):
+        res = NumericOrLogicIdWithBreak.findall("item 777, then @ a76, then id 777 and 123")
+        self.assertEqual(4, len(res))
+        r = res[0]
+        self.assertEqual((777, None, None), (r.numId, r.idTag, r.alphaNumId))
+        r = res[1]
+        self.assertEqual((None, "@", "a76"), (r.numId, r.idTag, r.alphaNumId))
+        r = res[2]
+        self.assertEqual((None, "id", "777"), (r.numId, r.idTag, r.alphaNumId))
+        r = res[3]
+        self.assertEqual((123, None, None), (r.numId, r.idTag, r.alphaNumId))
+
+
 if __name__ == '__main__':
     unittest.main()
