b906da3ba41ce142d955cf8d4d766f63dce6a68c
Hansi B <9629717+hansi-b@users.noreply.github.com>
2018-02-07 20:12:33 +0100

    added documentation
    * many doctest blocks as well

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ sys.path.insert(0, '/home/cb/Projects/reWrapped/src')
 # ones.
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
+    'sphinx.ext.doctest',
     'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
@@ -85,7 +86,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
+html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
